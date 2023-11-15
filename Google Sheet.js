5fb1f54bc066812af4161b19d0b60764d674fbcd
asementeira <138405257+asementeira@users.noreply.github.com>
2023-11-15 14:46:38 +0000

    newsletter tentativa
    newsletter tentativa

---
+++ Google Sheet.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzj_1GGnIggi4uzdd6eyn_drUfnipnhKI75JT4mn5jJrS-oTqIm0fGwJW91CiXZHjeiOQ/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+ e.preventDefault()
+ fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+ .then(response => alert("Thank you! your form is submitted successfully." ))
+ .then(() => { window.location.reload(); })
+ .catch(error => console.error('Error!', error.message))
+})
